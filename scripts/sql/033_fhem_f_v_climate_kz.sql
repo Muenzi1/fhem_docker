225baf0452c4fc659e4881a47792934af25bea07
Stephan Muenzberg <stephanmuenzberg@gmail.com>
2022-06-06 17:58:20 +0200

    Added view to get latest values stats.
    Renamings and a few view refactors were needed

---
+++ scripts/sql/033_fhem_f_v_climate_kz.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE VIEW fhem.f_v_climate_kz AS 
+SELECT 
+    facts.ID,
+    facts.TIMESTAMP,
+    facts.YEAR, 
+    facts.MONTH, 
+    facts.WEEK, 
+    facts.DAY, 
+    facts.VALUE,
+    facts.DASHBOARD,
+    facts.DISPLAY_NAME,
+    facts.READING,
+    facts.UNIT
+FROM fhem.f_v_climate AS facts
+WHERE DASHBOARD = 'climate_kz'
+;
